9cf3535f76bc6c26b3888d189555a404eaca3a15
Debkanta Samanta <debkanta484@gmail.com>
2024-02-23 10:49:46 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,64 @@
- export const registerController = () => {};
+import userModel from "../models/user Model.js";
+import {hashPassword} from "../helpers/authHelper.js";
+
+ export const registerController = async (req,res) => {
+  
+  try {
+    const { name, email, password, phone, address, answer } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+    if (!answer) {
+      return res.send({ message: "Answer is Required" });
+    }
+    //check user
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Registered please login",
+      });
+    }
+    //register user
+    const  hashPassword1 = await hashPassword (password);
+    console.log(hashPassword1)
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashPassword1,
+      answer,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+
+
+ };
